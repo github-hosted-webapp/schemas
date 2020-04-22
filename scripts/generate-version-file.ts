649ff262cc55dc060bbe021524a0637219c2b986
Rik Hoffbauer <62353999+rikhoffbauer@users.noreply.github.com>
2020-04-22 02:14:09 +0200

    chore: remove unnecessary line of code

--- scripts/generate-version-file.ts
+++ scripts/generate-version-file.ts
@@ -1,7 +1,5 @@
 import { generateVersionFile } from "./helpers/generateVersionFile";
 
-`export const version = "0.0.0" as const;`;
-
 generateVersionFile(process.env.VERSION_FILE_VERSION === "next")
     .then(() => console.log(`Version file generated successfully`))
     .catch(err => {
